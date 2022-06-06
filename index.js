726a8e80801cfbd2d49c5109f4339b8f282bb605
George Duncan <gtduncan@usc.edu>
2022-06-06 12:50:59 -0700

    commit tree 5729c5d4ee46fc00f7b7d9e84d6092707b6e41b4

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+function appendCat(name)
+{
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name)
+{
+    const newArray = [name, ...cats];
+    return newArray;
+}
+
+function removeLastCat()
+{
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat()
+{
+    return cats.slice(1)
+}
